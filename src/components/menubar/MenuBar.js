725131ec07d518713e53b8875489cc8c91725e60
terygasmen <terygasmen@gmail.com>
2023-10-08 17:29:11 +0800

    fixed menu bar icons

--- src/components/menubar/MenuBar.js
+++ src/components/menubar/MenuBar.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPen, faTable, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 function MenuBar({ onNewNoteClick, onTableViewClick }) {
   return (
     <div className="menu-bar">
       <div className="menu-item logo">
-        <img src='../../graphics/logo.png' alt='ZettelGospel Logo'/>
+        <img src='../../logo.svg' alt='ZettelGospel Logo'/>
       </div>
       <div className="menu-item" onClick={onNewNoteClick}>
-        <span role="img" aria-label="New Note">✍️</span>
+        <FontAwesomeIcon icon={faMagnifyingGlass}/>
+      </div>
+      <div className="menu-item" onClick={onNewNoteClick}>
+        <FontAwesomeIcon icon={faPen}/>  
       </div>
       <div className="menu-item" onClick={onTableViewClick}>
-        <span role="img" aria-label="Table View">⋮</span>
+        <FontAwesomeIcon icon={faTable}/>
       </div>
     </div>
   );
---
+++ styleguide.md
@@ -0,0 +1,19 @@
+# Fonts
+Roboto Mono - `@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;700&display=swap');`
+Weight: 400; 700;
+font-family: 'Roboto Mono', monospace;
+
+# Colors
+Black - #34373a
+White - #D8E1E9
+Gray - #44494e
+Light Gray - #c1c8ce
+
+# Icons
+home icon - `<i class="fa-sharp fa-solid fa-house" style="color: #c1c8ce;"></i>`
+search icon - `<i class="fa-sharp fa-solid fa-magnifying-glass" style="color: #c1c8ce;"></i>`
+pen icon - `<i class="fa-sharp fa-solid fa-pen" style="color: #c1c8ce;"></i>`
+table icon - `<i class="fa-sharp fa-solid fa-table" style="color: #c1c8ce;"></i>`
+settings icon - `<i class="fa-sharp fa-solid fa-gear" style="color: #c1c8ce;"></i>`
+plus icon - `<i class="fa-sharp fa-solid fa-plus" style="color: #c1c8ce;"></i>`
+minus icon - `<i class="fa-sharp fa-solid fa-minus" style="color: #c1c8ce;"></i>`
