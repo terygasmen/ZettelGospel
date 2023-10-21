1755ec0c060499b760ca0ee4752ffec7ae0f013a
terygasmen <terygasmen@gmail.com>
2023-10-21 19:49:05 +0800

    added keyboard shortcut to search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { FaSearch } from 'react-icons/fa';
 
 const Search = ({ handleSearchNote }) => {
+  const searchRef = useRef(null);
+
+  const handleKeyPress = (event) => {
+    // Check if Cmd (or Ctrl) key + K is pressed
+    if ((event.metaKey || event.ctrlKey) && event.key === 'k') {
+      // Focus the search input
+      searchRef.current.focus();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyPress);
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, []);
+
 	return (
 		<div className='search'>
 			<FaSearch className='search-icon' size='1.2em' />
@@ -11,6 +28,7 @@ const Search = ({ handleSearchNote }) => {
 				}
 				type='text'
 				placeholder='Cmd + K'
+        ref={searchRef}
 			/>
 		</div>
 	);
